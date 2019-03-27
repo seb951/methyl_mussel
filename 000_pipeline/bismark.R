abac06c38c54e851889611babb0eb4a74771ea68
Sébastien Renaut <renaut@cedar1.cedar.computecanada.ca>
2019-03-27 10:27:30 -0700

    new run

--- 000_pipeline/bismark.R
+++ 000_pipeline/bismark.R
@@ -31,18 +31,18 @@ for(i in 1:nrow(sequences))
   log = gsub(".gz",".trimmo.log",sequences[i,1])
   
  #trimmomatic
-  trimmo = paste("java -jar /home/renaut/bin/trimmomatic-0.36.jar PE -threads 1 -phred33 ",input_forward.fq, input_reverse.fq, output_forward_paired.fq, output_forward_unpaired.fq, output_reverse_paired.fq,output_reverse_unpaired.fq,"ILLUMINACLIP:/home/renaut/trinityrnaseq-Trinity-v2.8.4/trinity-plugins/Trimmomatic-0.36/adapters/all_primers.fa:2:30:10 SLIDINGWINDOW:4:15 MINLEN:59 2>",log,sep = "")
+  trimmo = paste("java -jar /home/renaut/bin/trimmomatic-0.36.jar PE -threads 16 -phred33 ",input_forward.fq, input_reverse.fq, output_forward_paired.fq, output_forward_unpaired.fq, output_reverse_paired.fq,output_reverse_unpaired.fq,"ILLUMINACLIP:/home/renaut/trinityrnaseq-Trinity-v2.8.4/trinity-plugins/Trimmomatic-0.36/adapters/all_primers.fa:2:30:10 SLIDINGWINDOW:4:15 MINLEN:59 2>",log,sep = "")
   
   system(trimmo)
   
   #prep genome
-  if(file.exists("00r_reference/Bisulfite_Genome") == F) {
-    genome_prep = c("/home/renaut/bismark_v0.20.1/bismark_genome_preparation  --path_to_bowtie /cvmfs/soft.computecanada.ca/easybuild/software/2017/avx2/Compiler/intel2016.4/bowtie2/2.3.4.1/bin/ --verbose 00r_reference")
+  if(file.exists("002_reference/Bisulfite_Genome") == F) {
+    genome_prep = c("/home/renaut/bismark_v0.20.1/bismark_genome_preparation --parallel 8 --path_to_bowtie /cvmfs/soft.computecanada.ca/easybuild/software/2017/avx2/Compiler/intel2016.4/bowtie2/2.3.4.1/bin/ --verbose 002_reference")
     system(genome_prep)
     }
 
   #Running bismark (Typical alignment example, tolerating one non-bisulfite mismatch per read)
-  bismark = paste("/home/renaut/bismark_v0.20.1/bismark --samtools_path /cvmfs/soft.computecanada.ca/easybuild/software/2017/avx2/Compiler/intel2016.4/samtools/1.9/bin/samtools --path_to_bowtie /cvmfs/soft.computecanada.ca/easybuild/software/2017/avx2/Compiler/intel2016.4/bowtie2/2.3.4.1/bin/ -n 1 --genome 00r_reference -1 ",output_forward_paired.fq," -2 ",output_reverse_paired.fq,sep = "")
+  bismark = paste("/home/renaut/bismark_v0.20.1/bismark -p 16 --samtools_path /cvmfs/soft.computecanada.ca/easybuild/software/2017/avx2/Compiler/intel2016.4/samtools/1.9/bin/samtools --path_to_bowtie /cvmfs/soft.computecanada.ca/easybuild/software/2017/avx2/Compiler/intel2016.4/bowtie2/2.3.4.1/bin/ -n 1 --genome 002_reference -1 ",output_forward_paired.fq," -2 ",output_reverse_paired.fq,sep = "")
   
   system(bismark)
 
@@ -64,11 +64,14 @@ for(i in 1:nrow(sequences))
  #move into a specific bismark folder
  mv = paste("mv *",strsplit(seq,"_R1")[[1]][1],"*  003_results/.",sep = "")
  system(mv)
+}
+
  
- # report
+ # report at the end....
  setwd("003_results/")
+
  report = paste("/home/renaut/bismark_v0.20.1/bismark2summary",sep = "")
  system(report)
  system("mv bismark_summary_report* ../.")
-}
+
 
